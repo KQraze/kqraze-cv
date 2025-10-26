9b177fb1f0ceeb1cde6adf5fd2f3d805b98002a9
KQraze <pavel.eliseev061@gmail.com>
2025-10-27 00:13:26 +0700

    init

---
+++ app/app.vue
@@ -0,0 +1,41 @@
+<script setup>
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'ru'
+  }
+})
+
+const title = 'Павел Елисеев - CV'
+const description = 'Pavel Eliseev CV, Full Stack Developer, JavaScript, Vue.js, Nuxt.js, Node.js, Portfolio'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: '',
+  twitterImage: '',
+  twitterCard: ''
+})
+</script>
+
+<template>
+  <UApp>
+    <UHeader title="Павел Елисеев">
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+
+    <UFooter>
+
+    </UFooter>
+  </UApp>
+</template>
